0a0977c388965d84dae11b30d8ade6b0fa4894b2
Daniel Torres <denial.torres@gmail.com>
2017-03-08 09:14:30 -0700

    json request

--- lib/mifiel/signer.rb
+++ lib/mifiel/signer.rb
@@ -3,6 +3,12 @@ module Mifiel
     put :add, '/documents/:id/signers'
     def self.create
       puts "estas en signer"
+      rest_request = RestClient::Request.new(
+        url: "#{Mifiel.config.base_url}/documents/#{id}/signers/",
+        method: :post,
+      )
+      req = ApiAuth.sign!(rest_request, Mifiel.config.app_id, Mifiel.config.app_secret)
+      req.execute
     end
     
     
