2383550cd4e92b7e3e31d386636b2f2fc8d5ca55
Daniel Torres <denial.torres@gmail.com>
2017-03-08 18:15:04 -0700

    test 8

--- lib/mifiel/signer.rb
+++ lib/mifiel/signer.rb
@@ -4,7 +4,7 @@ module Mifiel
   get :all, "/signers"  
   get :find, "/signers/:id"
   put :save, "/signers/:id"
-  post :create, "/documents/:id/signers"
+  post :create, "/documents/291a9989-992f-44b1-9a37-8eff350d644c/signers"
 
   puts 'judios raros'
   end
