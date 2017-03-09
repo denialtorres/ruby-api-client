8699d1daaa2b5b9f9b318a246944785cf306ffd2
Daniel Torres <denial.torres@gmail.com>
2017-03-08 18:08:03 -0700

    test 7

--- lib/mifiel/signer.rb
+++ lib/mifiel/signer.rb
@@ -1,17 +1,11 @@
 module Mifiel
   class Signer < Mifiel::Base
-  put :add, '/documents/:id/signers'
+  puts 'estas en la nueva vida'
+  get :all, "/signers"  
+  get :find, "/signers/:id"
+  put :save, "/signers/:id"
+  post :create, "/documents/:id/signers"
 
-    def self.create
-      puts "estas en signer"
-      rest_request = RestClient::Request.new(
-        url: "#{Mifiel.config.base_url}/documents/291a9989-992f-44b1-9a37-8eff350d644c/signers",
-        method: :post,
-        params: { email: 'correo@prueba.com' },
-        ssl_version: 'SSLv23'
-      )
-      req = ApiAuth.sign!(rest_request, Mifiel.config.app_id, Mifiel.config.app_secret)
-      req.execute
-    end
+  puts 'judios raros'
   end
 end
